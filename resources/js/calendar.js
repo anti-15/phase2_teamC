c5dc02ad06f5939bce1ff6309bea50798dd47f0f
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-12 13:52:16 +0900

    ブラウザ上でのイベント入力

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -17,7 +17,19 @@ let calendar = new Calendar(calendarEl, {
     // 日付をクリック、または範囲を選択したイベント
     selectable: true,
     select: function (info) {
-        alert("selected " + info.startStr + " to " + info.endStr);
+        //alert("selected " + info.startStr + " to " + info.endStr);
+        //入力ダイアログ
+        const eventName = prompt("イベントを入力してください");
+        
+        if (eventName) {
+            // イベントの追加
+            calendar.addEvent({
+                title: eventName,
+                start: info.start,
+                end: info.end,
+                allDay: true,
+            });
+        }
     },
 });
 calendar.render();
