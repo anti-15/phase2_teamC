013974d8f5a1544630c582121c9578ba8d9410d7
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-12 16:53:41 +0900

    検証リポジトリから移植（動くとは言っていない）

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -9,31 +9,102 @@ import listPlugin from "@fullcalendar/list";
 var calendarEl = document.getElementById("calendar");
 
 let calendar = new Calendar(calendarEl, {
-    plugins: [interactionPlugin,dayGridPlugin,timeGridPlugin,listPlugin],
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
     initialView: "dayGridMonth",
+    editable: true,
     headerToolbar: {
         left: "prev,next today",
         center: "title",
         right: "dayGridMonth,timeGridWeek,listWeek",
     },
-    locale: "ja",
-
-    // 日付をクリック、または範囲を選択したイベント
+    events: '/full-calendar',
     selectable: true,
-    select: function (info) {
-        //alert("selected " + info.startStr + " to " + info.endStr);
-        //入力ダイアログ
-        const eventName = prompt("イベントを入力してください");
-        
-        if (eventName) {
-            // イベントの追加
-            calendar.addEvent({
-                title: eventName,
-                start: info.start,
-                end: info.end,
-                allDay: true,
-            });
+    selectHelper: true,
+    locale: "ja",
+    select: function (start, end, allDay) {
+        var title = prompt('Event Title:');
+
+        if (title) {
+            var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+
+            var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
+
+            $.ajax({
+                url: "/full-calendar/action",
+                type: "POST",
+                data: {
+                    title: title,
+                    start: start,
+                    end: end,
+                    type: 'add'
+                },
+                success: function (data) {
+                    calendar.fullCalendar('refetchEvents');
+                    alert("Event Created Successfully");
+                }
+            })
         }
     },
+    editable: true,
+    eventResize: function (event, delta) {
+        var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+        var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+        var title = event.title;
+        var id = event.id;
+        $.ajax({
+            url: "/full-calendar/action",
+            type: "POST",
+            data: {
+                title: title,
+                start: start,
+                end: end,
+                id: id,
+                type: 'update'
+            },
+            success: function (response) {
+                calendar.fullCalendar('refetchEvents');
+                alert("Event Updated Successfully");
+            }
+        })
+    },
+    eventDrop: function (event, delta) {
+        var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+        var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+        var title = event.title;
+        var id = event.id;
+        $.ajax({
+            url: "/full-calendar/action",
+            type: "POST",
+            data: {
+                title: title,
+                start: start,
+                end: end,
+                id: id,
+                type: 'update'
+            },
+            success: function (response) {
+                calendar.fullCalendar('refetchEvents');
+                alert("Event Updated Successfully");
+            }
+        })
+    },
+
+    eventClick: function (event) {
+        if (confirm("Are you sure you want to remove it?")) {
+            var id = event.id;
+            $.ajax({
+                url: "/full-calendar/action",
+                type: "POST",
+                data: {
+                    id: id,
+                    type: "delete"
+                },
+                success: function (response) {
+                    calendar.fullCalendar('refetchEvents');
+                    alert("Event Deleted Successfully");
+                }
+            })
+        }
+    }
 });
 calendar.render();
