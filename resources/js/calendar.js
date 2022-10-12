0884cc96aa1536ba20816efbd8a1f6dff7bcdaa5
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-12 17:33:43 +0900

    予期したエラーになるように

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -4,7 +4,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
-
+import { formatDate } from '@fullcalendar/core'
 
 var calendarEl = document.getElementById("calendar");
 
@@ -21,90 +21,96 @@ let calendar = new Calendar(calendarEl, {
     selectable: true,
     selectHelper: true,
     locale: "ja",
-    select: function (start, end, allDay) {
+    select: async function (start, end, allDay) {
         var title = prompt('Event Title:');
 
         if (title) {
-            var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+            var start = formatDate(start, 'Y-MM-DD HH:mm:ss')
+            var end = formatDate(end, 'Y-MM-DD HH:mm:ss')
 
-            var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
-
-            $.ajax({
-                url: "/full-calendar/action",
-                type: "POST",
-                data: {
-                    title: title,
-                    start: start,
-                    end: end,
-                    type: 'add'
-                },
-                success: function (data) {
-                    calendar.fullCalendar('refetchEvents');
-                    alert("Event Created Successfully");
+            try {
+                const response = await fetch("/full-calendar/action", {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        title: title,
+                        start: start,
+                        end: end,
+                        type: 'add'
+                    })
+                })
+                if (!response.ok) {
+                    console.error('response.ok:', response.ok);
+                    console.error('esponse.status:', response.status);
+                    console.error('esponse.statusText:', response.statusText);
+                    throw new Error(response.statusText);
                 }
-            })
+                calendar.refetchEvents();
+                alert("Event Created Successfully");
+            } catch (error) {
+                alert(`Error: ${error}`);
+            }
         }
     },
-    editable: true,
-    eventResize: function (event, delta) {
-        var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-        var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-        var title = event.title;
-        var id = event.id;
-        $.ajax({
-            url: "/full-calendar/action",
-            type: "POST",
-            data: {
-                title: title,
-                start: start,
-                end: end,
-                id: id,
-                type: 'update'
-            },
-            success: function (response) {
-                calendar.fullCalendar('refetchEvents');
-                alert("Event Updated Successfully");
-            }
-        })
-    },
-    eventDrop: function (event, delta) {
-        var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-        var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-        var title = event.title;
-        var id = event.id;
-        $.ajax({
-            url: "/full-calendar/action",
-            type: "POST",
-            data: {
-                title: title,
-                start: start,
-                end: end,
-                id: id,
-                type: 'update'
-            },
-            success: function (response) {
-                calendar.fullCalendar('refetchEvents');
-                alert("Event Updated Successfully");
-            }
-        })
-    },
+    // editable: true,
+    // eventResize: function (event, delta) {
+    //     var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+    //     var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+    //     var title = event.title;
+    //     var id = event.id;
+    //     $.ajax({
+    //         url: "/full-calendar/action",
+    //         type: "POST",
+    //         data: {
+    //             title: title,
+    //             start: start,
+    //             end: end,
+    //             id: id,
+    //             type: 'update'
+    //         },
+    //         success: function (response) {
+    //             calendar.fullCalendar('refetchEvents');
+    //             alert("Event Updated Successfully");
+    //         }
+    //     })
+    // },
+    // eventDrop: function (event, delta) {
+    //     var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+    //     var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+    //     var title = event.title;
+    //     var id = event.id;
+    //     $.ajax({
+    //         url: "/full-calendar/action",
+    //         type: "POST",
+    //         data: {
+    //             title: title,
+    //             start: start,
+    //             end: end,
+    //             id: id,
+    //             type: 'update'
+    //         },
+    //         success: function (response) {
+    //             calendar.fullCalendar('refetchEvents');
+    //             alert("Event Updated Successfully");
+    //         }
+    //     })
+    // },
 
-    eventClick: function (event) {
-        if (confirm("Are you sure you want to remove it?")) {
-            var id = event.id;
-            $.ajax({
-                url: "/full-calendar/action",
-                type: "POST",
-                data: {
-                    id: id,
-                    type: "delete"
-                },
-                success: function (response) {
-                    calendar.fullCalendar('refetchEvents');
-                    alert("Event Deleted Successfully");
-                }
-            })
-        }
-    }
+    // eventClick: function (event) {
+    //     if (confirm("Are you sure you want to remove it?")) {
+    //         var id = event.id;
+    //         $.ajax({
+    //             url: "/full-calendar/action",
+    //             type: "POST",
+    //             data: {
+    //                 id: id,
+    //                 type: "delete"
+    //             },
+    //             success: function (response) {
+    //                 calendar.fullCalendar('refetchEvents');
+    //                 alert("Event Deleted Successfully");
+    //             }
+    //         })
+    //     }
+    // }
 });
 calendar.render();
