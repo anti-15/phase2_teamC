e9bbc8fea5cee2b70852b18c1f9351740f347e56
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-12 13:43:23 +0900

    日の範囲を選択

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,16 +1,23 @@
 import '@fullcalendar/core/vdom'; // for Vite
 import { Calendar } from "@fullcalendar/core";
+import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
 var calendarEl = document.getElementById("calendar");
 
 let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
+    plugins: [interactionPlugin,dayGridPlugin],
     initialView: "dayGridMonth",
     headerToolbar: {
         left: "prev,next today",
         center: "title",
         right: "",
+    },
+
+    // 日付をクリック、または範囲を選択したイベント
+    selectable: true,
+    select: function (info) {
+        alert("selected " + info.startStr + " to " + info.endStr);
     },
 });
 calendar.render();
