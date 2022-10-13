6ffff7b9f655121e768f5b128c337c738ed0362e
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-14 01:41:18 +0900

    不要なコードを削除

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,4 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class=" text-xl text-gray-800 leading-tight text-center font-bold">
-    </h2>
-  </x-slot>
-        <script>
-          $(document).ready(function () {
-              $.ajaxSetup({
-              headers:{
-                  'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-              }
-          });
-        </script>
-          <a href="{{ route('schedule.create') }}">create</a>
-          <head>
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        </head>
-        <div id='calendar'></div>
+  <a href="{{ route('schedule.create') }}">create</a>
+  <div id='calendar'></div>
 </x-app-layout>
