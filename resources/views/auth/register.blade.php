c2c88d422c3d776b00c048c2f3b2e6c2cab40cae
Yanai1005 <tatsuya871_1005@icloud.com>
2022-10-16 18:04:18 +0900

    登録の時色を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,18 +14,27 @@
                 <x-input-label for="name" :value="__('Name')" />
 
                 <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-
+                
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
-            </div>
+                </div>
+                
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                    
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    
+                    <!-- color -->
+                    <div class="mt-4">
+                        <x-input-label for="color" :value="__('Color')" />
+                        
+                        <input id="color" class="block mt-1 w-full" type="color" name="color"  required />
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
+                        <x-input-error :messages="$errors->get('color')" class="mt-2" />
+                    </div>
 
             <!-- Password -->
             <div class="mt-4">
