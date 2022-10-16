ac4b53aea659503d605d1c4b629ae0c2404dc4c3
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2022-10-16 15:59:25 +0900

    特定のユーザーのみ削除可能

--- resources/views/schedule_detail.blade.php
+++ resources/views/schedule_detail.blade.php
@@ -38,6 +38,7 @@
              <a href="{{ url()->previous() }}" class="block text-center w-full py-3 m-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
               Back
             </a>
+            @if($schedule->user_id == Auth::user()->id)
                <form action="{{ route('schedule.destroy',$schedule->id) }}" method="POST" class="text-left">
                       @method('delete')
                       @csrf
@@ -46,7 +47,8 @@
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                         </svg>
                       </button>
-                    </form>           
+                    </form>  
+                    @endif        
             </div>
           </div>
         </div>
