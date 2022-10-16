2024748919b135349b56a62c3194d5b5efab142e
ramenumai <taiki1151001@icloud.com>
2022-10-16 16:15:41 +0900

    ダイアログメッセージ

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,4 @@ Alpine.start();
 
 import './calendar';
 
-function delete_alert(e){
-    if(!window.confirm('本当に削除しますか？')){
-        window.alert('キャンセルされました'); 
-        return false;
-    }
-    document.deleteform.submit();
-};
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,11 @@
       {{ $group_id }}
     </h2>
   </x-slot>
-            <form class="mb-6" action="{{ route('group.destroy', $group_id) }}" method="POST">
+            <form onClick= "return confirm('本当に退会しますか？')" class="mb-6" action="{{ route('group.destroy', $group_id) }}" method="POST">
               @method('delete')
               @csrf
             <div class="flex flex-col mb-4">
-              <button type="submit" class="w-full py-6 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+              <button  type="submit" class="w-full py-6 mt-6 mb-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
               グループを退会する
               </button>
             </div>
